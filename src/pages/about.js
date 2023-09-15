d9e2b12365f3d4d359004e5353255b9b6f42d142
topsecretagent008 <secretagent1025@gmail.com>
2023-09-15 14:57:49 +0900

    add FAQ

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import '../App.css';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FiMinus, FiPlus } from "react-icons/fi";
 
 export default function About() {
@@ -7,6 +8,8 @@ export default function About() {
   const [currentDropDown2, setCurrentDropDown2] = useState(false);
   const [currentDropDown3, setCurrentDropDown3] = useState(false);
   const [currentDropDown4, setCurrentDropDown4] = useState(false);
+  const [currentDropDown5, setCurrentDropDown5] = useState(false);
+
 
   const dropDownText1 = async () => {
     await dropDownText(1);
@@ -28,27 +31,42 @@ export default function About() {
     await setCurrentDropDown4(!currentDropDown4)
   }
 
+  const dropDownText5 = async () => {
+    await dropDownText(5);
+    await setCurrentDropDown5(!currentDropDown5)
+  }
+
   const dropDownText = (e) => {
     switch (e) {
       case 1:
         setCurrentDropDown2(false);
         setCurrentDropDown3(false);
         setCurrentDropDown4(false);
+        setCurrentDropDown5(false);
         break;
       case 2:
         setCurrentDropDown1(false);
         setCurrentDropDown3(false);
         setCurrentDropDown4(false);
+        setCurrentDropDown5(false);
         break;
       case 3:
         setCurrentDropDown1(false);
         setCurrentDropDown2(false);
         setCurrentDropDown4(false);
+        setCurrentDropDown5(false);
         break;
       case 4:
         setCurrentDropDown1(false);
         setCurrentDropDown2(false);
         setCurrentDropDown3(false);
+        setCurrentDropDown5(false);
+        break;
+      case 5:
+        setCurrentDropDown1(false);
+        setCurrentDropDown2(false);
+        setCurrentDropDown3(false);
+        setCurrentDropDown4(false);
         break;
       default:
         break;
@@ -56,7 +74,7 @@ export default function About() {
   }
 
   return (
-    <div className="w-full mx-auto py-5 lg:py-0 lg:h-screen max-w-[1200px]">
+    <div className="w-full mx-auto py-5 lg:py-0  h-full min-h-screen max-w-[1200px]">
       <div className='gap-10 justify-center text-start flex flex-col lg:flex-row mt-44 px-6'>
         <div className='w-full lg:w-1/2 justify-start mb-12 gap-10 flex flex-col'>
           <div className='text-white font-bold text-3xl'>
@@ -134,6 +152,32 @@ export default function About() {
             <div className={`${currentDropDown4 ? "py-[24px]" : ""} px-[29px] text-start text-white/60 bg-[#ffffff0d] backdrop-filter duration-300`}>
               <span className={`${currentDropDown4 ? " h-full flex" : "h-0 hidden"}`}>
                 Royalties from secondary sales will be paid back equally among the artists who so kindly joined us in our effort.
+              </span>
+            </div>
+            <div onClick={() => dropDownText5()} className='cursor-pointer text-white/60 hover:text-white bg-[#ffffff0d] backdrop-filter flex items-center justify-between px-[30px] py-[24px] font-bold text-[18px] lg:text-[24px] mt-[10px] '>
+              Where will the funds be donated?
+              <div className='text-[24px]'>
+                {currentDropDown5 ?
+                  <FiMinus />
+                  :
+                  <FiPlus />}
+              </div>
+            </div>
+            <div className={`${currentDropDown5 ? "py-[24px]" : ""} px-[29px] text-start text-white/60 bg-[#ffffff0d] backdrop-filter duration-300`}>
+              <span className={`${currentDropDown5 ? " h-full flex flex-col gap-4" : "h-0 hidden"}`}>
+                We are excited to donate all proceeds to the Iranian Diaspora Collective’s “Keep Iran Online” initiative. Journalists are not allowed in Iran. Thus, social media is the only way Iranians can document and disseminate what’s really happening in Iran. In the face of regime crackdowns on internet connectivity, it is of paramount importance that we protect the connectivity of the young Iranians who are leading this movement.
+                <p className='break-all list-inside'>
+                  You can learn more at {' '}
+                  <span>
+                    <Link
+                      className="text-blue-600 underline decoration-sky-500/30 break-words"
+                      href="https://www.iraniandiasporacollective.com/connectivity"
+                      target="_blank"
+                    >
+                      iraniandiasporacollective.com
+                    </Link >
+                  </span>.
+                </p>
               </span>
             </div>
           </div>
